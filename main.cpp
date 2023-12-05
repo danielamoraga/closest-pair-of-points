d8d01c1025db961bb8fc0efe7144abc80a5d8e03
Gonzalo <gonzalo.cisternas@ug.uchile.cl>
2023-12-05 16:00:33 -0300

    Co-authored-by: dani <danielamoraga@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -2,18 +2,7 @@
 using namespace std;
 
 
-int main()
-{
-
-	// Points on a plane P[i] = {x, y}
-	vector<pair<int, int> > P = {
-		{ 1, 2 }, { 2, 3 }, { 3, 4 }, { 5, 6 }, { 2, 1 }
-	};
-	int n = P.size();
+int main() {
 	
-	// Function call
-	ClosestPairResult result = closestPair(P, n);
-    cout << "The smallest distance is " << result.distance << endl;
-    cout << "The closest pair of points are (" << result.point1.first << ", " << result.point1.second << ") and (" << result.point2.first << ", " << result.point2.second << ")" << endl;
 	return 0;
 }
--- sweepLineExp.cpp
+++ sweepLineExp.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <chrono>
 #include <thread>
 #include <vector>
@@ -22,7 +23,7 @@ en sus implementacionesse asemejan a lo conocido por la teoría.*/
 
 
 // Mide la ejecución de un algoritmo para un valor de n, generando n puntos aleatorios
-void measureExecutionTime(int n) {
+void measureExecutionTime(int n, int iterations, ofstream &myfile) {
     
     vector<pair<float, float>> coordinates;
     
@@ -34,6 +35,7 @@ void measureExecutionTime(int n) {
     // Genera n puntos aleatorios
     for (int i = 0; i < n; i++) {
         coordinates.push_back({dis(g), dis(g)});
+        // cout << "(" << coordinates[i].first << " ," << coordinates[i].second << ")" << endl;
     }
 
     // Run the algorithm and measure the execution time
@@ -45,35 +47,23 @@ void measureExecutionTime(int n) {
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 
     // Print the execution time
-    std::cout << "Execution time for n = " << n << ": " << duration << " milliseconds" << std::endl;
+    cout << iterations << " " << duration << endl;
+    // Write the execution time in the file
+    myfile << iterations << " " << duration << endl;
 }
 
 int main() {
-    // Create a vector to store the threads
-    std::vector<std::thread> threads;
+    //Abre un archivo para escribir los resultados
+    ofstream myfile;
+    myfile.open ("results.txt");
+    long int n = 5000000;
+    int iterations = 10;
+    cout << "Starting the execution" << endl;
+    for (int i = 0; i < iterations; i++) {
+        cout << "Iteration " << i << endl;
+        measureExecutionTime(n, iterations, myfile);
 
-    // Define the range of n values
-    int startN = 0;
-    int endN = 99;
-    int step = 1;
-
-    // Create 9 threads to measure the execution time for each algorithm
-    for (int n = startN; n <= endN; n += step) {
-        cout<<"n: "<<n<<endl;
-        threads.emplace_back(measureExecutionTime, n);
-        if (threads.size() == 100) {
-            // Wait for all threads to finish
-            for (auto& thread : threads) {
-                thread.join();
-            }
-            threads.clear();
-        }
     }
-
-    // Wait for the remaining threads to finish
-    for (auto& thread : threads) {
-        thread.join();
-    }
-
+    myfile.close();
     return 0;
 }
